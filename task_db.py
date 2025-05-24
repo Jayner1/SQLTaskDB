348032772fabd2b6c79db7d227bd7ecbf5259c6b
Jayner1 <jasontyale@gmail.com>
2025-05-24 10:48:17 -0700

    2

--- task_db.py
+++ task_db.py
@@ -1,7 +1,5 @@
 import sqlite3
-def view_tasks(conn):
-    cursor = conn.cursor()
-    cursor.execute("SELECT t.task_id, t.description, t.is_completed, c.category_name FROM Tasks t JOIN Categories c ON t.category_id = c.category_id")
-    for task in cursor.fetchall():
-        status = "Yes" if task[2] else "No"
-        print(f"ID: {task[0]}, Description: {task[1]}, Category: {task[3]}, Completed: {status}")
+print(sqlite3.version)
+conn = sqlite3.connect(":memory:")
+print("Connected!")
+conn.close()
