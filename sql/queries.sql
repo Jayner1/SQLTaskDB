91e53aed508761ac2c7f1217b68bb837efcb1299
Jayner1 <jasontyale@gmail.com>
2025-05-24 12:08:37 -0700

    updates readme, queries, updates

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,16 +1,19 @@
--- Lists all tasks with category names
-SELECT t.task_id, t.description, t.is_completed, c.category_name
+-- Lists all tasks with category and priority names
+SELECT t.task_id, t.description, t.is_completed, c.category_name, p.priority_name
 FROM Tasks t
-JOIN Categories c ON t.category_id = c.category_id;
+JOIN Categories c ON t.category_id = c.category_id
+JOIN Priority p ON t.priority_id = p.priority_id;
 
--- Lists completed tasks
+-- Lists completed tasks with categories and priorities
+SELECT t.description, c.category_name, p.priority_name
+FROM Tasks t
+JOIN Categories c ON t.category_id = c.category_id
+JOIN Priority p ON t.priority_id = p.priority_id
+WHERE t.is_completed = 1;
+
+-- Lists high-priority tasks
 SELECT t.description, c.category_name
 FROM Tasks t
 JOIN Categories c ON t.category_id = c.category_id
-WHERE t.is_completed = 1;
-
--- Lists tasks in School category
-SELECT t.description
-FROM Tasks t
-JOIN Categories c ON t.category_id = c.category_id
-WHERE c.category_name = 'School';
+JOIN Priority p ON t.priority_id = p.priority_id
+WHERE p.priority_name = 'High';
--- sql/updates.sql
+++ sql/updates.sql
@@ -1,5 +1,8 @@
 -- Marks task as complete by task_id
 UPDATE Tasks SET is_completed = 1 WHERE task_id = 1;
 
+-- Updates task priority by task_id
+UPDATE Tasks SET priority_id = 3 WHERE task_id = 2;
+
 -- Deletes task by task_id
 DELETE FROM Tasks WHERE task_id = 2;
